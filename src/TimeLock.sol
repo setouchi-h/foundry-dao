0b87de724f379e509b1043cf53b24bc5370e19f6
setouchi-h <setouchi.kazuki@gmail.com>
2023-10-29 03:59:29 +0900

    second commit

---
+++ src/TimeLock.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract TimeLock is TimelockController {
+    /**
+     * @param minDelay how long you have to wait before executing
+     * @param proposers the list of addresses that can propose
+     * @param executors the list of addresses that can execute
+     */
+    constructor(uint256 minDelay, address[] memory proposers, address[] memory executors)
+        TimelockController(minDelay, proposers, executors, msg.sender)
+    {}
+}
---
+++ test/MyGovernorTest.t.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {MyGovernor} from "../src/MyGovernor.sol";
+import {Box} from "../src/Box.sol";
+import {TimeLock} from "../src/TimeLock.sol";
+import {GovToken} from "../src/GovToken.sol";
+
+contract MyGovernorTest is Test {
+    MyGovernor governor;
+    Box box;
+    TimeLock timelock;
+    GovToken govToken;
+
+    address public USER = makeAddr("user");
+    uint256 public constant INITIAL_SUPPLY = 100 ether;
+
+    address[] proposers;
+    address[] executors;
+    uint256 public constant MIN_DELAY = 1 hours;
+
+    function setUp() public {
+        govToken = new GovToken();
+        govToken.mint(USER, INITIAL_SUPPLY);
+
+        vm.startPrank(USER);
+        govToken.delegate(USER);
+        timelock = new TimeLock(MIN_DELAY, proposers, executors);
+        governor = new MyGovernor(govToken, timelock);
+
+        bytes32 proposerRole = timelock.PROPOSER_ROLE();
+        bytes32 executorRole = timelock.EXECUTOR_ROLE();
+        bytes32 adminRole = timelock.DEFAULT_ADMIN_ROLE();
+
+        timelock.grantRole(proposerRole, address(governor));
+        timelock.grantRole(executorRole, address(0));
+        timelock.revokeRole(adminRole, USER);
+        vm.stopPrank();
+
+        box = new Box();
+        box.transferOwnership(address(timelock));
+    }
+}
